ef136276117c255c92d79f25f17069a7cec407e1
Ben Galewsky <ben@peartreestudio.net>
2023-03-09 14:01:39 -0600

    Extract data from the procedures and thermochemical tabs

--- experiment-from-excel/remat.experiment_from_excel.py
+++ experiment-from-excel/remat.experiment_from_excel.py
@@ -14,6 +14,62 @@ import pyclowder.files
 from openpyxl import load_workbook
 
 
+def read_inputs_from_worksheet(ws: Worksheet) -> dict:
+    inputs = []
+    headers = [col.value for col in list(ws.rows)[0]]
+    for row in ws.iter_rows(min_row=2):
+        input_properties = {key: cell.value for key, cell in zip(headers, row)}
+        inputs.append(input_properties)
+
+    return inputs
+
+
+def read_procedure_from_worksheet(ws: Worksheet) -> dict:
+    procedure = {}
+    for row in ws.iter_rows():
+        procedure[row[0].value] = row[1].value
+
+    return procedure
+
+
+def read_thermochemical_from_worksheet(ws: Worksheet) -> dict:
+    values = {}
+    for row in ws.iter_rows():
+        if row[0].style == "CurePhase":
+            cure_phase = row[0].value
+            values[cure_phase] = {
+                "Delta HR Baseline": {}
+            }
+        elif row[0].style == "DeltaHRBaseline":
+            pass
+        elif row[0].style == "DeltaHRBaselineVal":
+            property_name = row[0].value
+            values[cure_phase]["Delta HR Baseline"][property_name] = row[1].value
+        else:
+            property_name = row[0].value
+            values[cure_phase][property_name] = row[1].value
+    return values
+
+
+def excel_to_json(path):
+    wb = load_workbook(filename=path)
+    inputs = {}
+
+    for sheet in wb.sheetnames:
+        if sheet == "thermochemical":
+            thermochemical = read_thermochemical_from_worksheet(wb[sheet])
+        elif sheet == "procedure":
+            procedure = read_procedure_from_worksheet(wb[sheet])
+        else:
+            inputs[sheet] = read_inputs_from_worksheet(wb[sheet])
+
+    return {
+        "procedure": procedure,
+        "inputs": inputs,
+        "thermochemical": thermochemical
+    }
+
+
 class ExperimentFromExcel(Extractor):
     def __init__(self):
         Extractor.__init__(self)
@@ -28,23 +84,9 @@ class ExperimentFromExcel(Extractor):
     def check_message(self, connector, host, secret_key, resource, parameters):
         return CheckMessage.download
 
-    def excel_to_json(self, path):
-        wb = load_workbook(filename=path)
-        inputs = {}
-
-        for sheet in wb.sheetnames:
-            inputs[sheet] = []
-            ws = wb[sheet]
-            headers = [col.value for col in list(ws.rows)[0]]
-            for row in ws.iter_rows(min_row=2):
-                input_properties = {key: cell.value for key, cell in zip(headers, row)}
-                inputs[sheet].append(input_properties)
-
-        return {"inputs": inputs}
-
     def process_message(self, connector, host, secret_key, resource, parameters):
         logger = logging.getLogger('__main__')
-        experiment = self.excel_to_json(resource['local_paths'][0])
+        experiment = excel_to_json(resource['local_paths'][0])
         logger.debug(experiment)
 
         # store results as metadata
