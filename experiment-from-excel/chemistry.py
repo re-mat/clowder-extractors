9803ffae6d2fc32393c7369c56a444dad74d76c1
Ben Galewsky <ben@peartreestudio.net>
2023-03-28 16:07:36 -0500

    Start populating spreadsheet values

--- experiment-from-excel/chemistry.py
+++ experiment-from-excel/chemistry.py
@@ -9,8 +9,13 @@ class ChemDB:
     def load_database(self):
         self.data = pandas.read_csv("https://uofi.box.com/shared/static/p8r6ef1lcj0lk44ggcb6zmv1d66abyfk.csv", index_col="SMILES")
 
-    def exists(self, smiles: str) -> bool:
-        return smiles in self.data.index.to_list()
+    def exists(self, smiles: str | list) -> bool:
+        if type(smiles) == str:
+            return smiles in self.data.index.to_list()
+        else:
+            for test_smiles in smiles:
+                if not self.exists(test_smiles):
+                    raise ValueError(f"{test_smiles} not in Chemistry Database")
 
     def density(self, smiles) -> float:
         return self.data.at[smiles, "Density (g/mL)"]
@@ -28,6 +33,10 @@ class ChemistryConverter:
         self.molecular_weight = db.molecular_weight(smiles)
         self.density = db.density(smiles)
 
+        mass = None if mass == "-" else mass
+        volume = None if volume == "-" else volume
+
+
         if not mass and not volume:
             raise ValueError("Volume or mass must be specified")
 
@@ -38,6 +47,9 @@ class ChemistryConverter:
             self.mass = mass
         else:
             self.mass = self.mass_from_volume(volume)
+
+    def __repr__(self):
+        return f"{type(self)} {self.smiles} - Mass: {self.mass}, Volume: {self.volume()}, Moles {self.moles()}"
 
     def mass_from_volume(self, volume: float) -> float:
         return volume * self.density
@@ -67,7 +79,7 @@ class Monomer(ChemistryConverter):
             M_avg += X_i * monomer.molecular_weight
         return M_avg
 
-class Catalyst(Monomer):
+class Catalyst(ChemistryConverter):
     def __init__(self, smiles: str, db: ChemDB, mass=None, volume=None):
         super().__init__(smiles, db, mass, volume)
         if mass:
