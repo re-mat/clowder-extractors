0ce00e74909c829e5bc28fdf4fc731fc910ca19a
Ben Galewsky <ben@peartreestudio.net>
2023-03-29 17:21:52 -0500

    Oops Filler

--- experiment-from-excel/remat.experiment_from_excel.py
+++ experiment-from-excel/remat.experiment_from_excel.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import json
 
-from chemistry import Monomer, ChemDB, Catalyst, Inhibitor, Filler, Solvent, Additive
+from chemistry import Monomer, ChemDB, Catalyst, Inhibitor, Solvent, Additive
 import logging
 
 import pyclowder.files
